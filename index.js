d7405dd4c2cebc4bc1ab956718ba8a7a7c519bb8
chujumanganese <uemmanuel911@gmail.com>
2026-02-14 10:41:35 -0800

    the second

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import sqlite3 from "sqlite3";
 import dotenv from "dotenv";
 
 const app = express();
+const db = sqlite3.verbose();
+
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
@@ -9,9 +12,31 @@ app.use(express.static("./public"));
 
 var PORT = 8000;
 
+// database part of the code
+const database = new db.Database("./user.db");
+let sql_one = `CREATE TABLE USERS(
+
+)`
+// database.run()
 
 app.get("/", (req, res)=>{
     res.render("home");
+})
+
+app.get("/login", (req, res)=>{
+    res.render("login");
+})
+
+app.get("/register", (req, res)=>{
+    res.render("register");
+})
+
+app.get("/dashboard", (req, res)=>{
+    res.render("dashboard");
+})
+
+app.get("/link", (req, res)=>{
+    res.render("link_account");
 })
 
 app.listen(PORT, ()=>{

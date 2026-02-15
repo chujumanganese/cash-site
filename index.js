9d68dc9e0ba928f4637244f55428aa4c0468fcb7
chujumanganese <uemmanuel911@gmail.com>
2026-02-14 18:41:56 -0800

    third part

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import sqlite3 from "sqlite3";
+import {body, validationResult} from "express-validator";
 import dotenv from "dotenv";
 
 const app = express();
@@ -7,6 +8,8 @@ const db = sqlite3.verbose();
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("./public"));
 
@@ -14,10 +17,25 @@ var PORT = 8000;
 
 // database part of the code
 const database = new db.Database("./user.db");
-let sql_one = `CREATE TABLE USERS(
 
-)`
-// database.run()
+function tables(){
+    let sql_one = `CREATE TABLE IF NOT EXISTS USERS(
+        id integer primary key AUTOINCREMENT , 
+        email varchar(200),
+        password varchar(255),
+        chaining varchar(150),
+        dates DATETIME DEFAULT CURRENT_TIMESTAMP
+    )`;
+
+    database.run(sql_one, (err) => {
+        if (err) {
+            console.log("The error is =>", err);
+        } else {
+            console.log("The users table is created");
+        }
+    });
+
+}tables();
 
 app.get("/", (req, res)=>{
     res.render("home");
@@ -28,8 +46,27 @@ app.get("/login", (req, res)=>{
 })
 
 app.get("/register", (req, res)=>{
-    res.render("register");
+    res.render("register", {errors: 0, old: ""});
 })
+
+// dashboard part of the code
+
+const userValidationRules = [
+    body('emails').isEmail().withMessage('Valid email is required'),
+    body('passwords').isLength({ min: 6 }).withMessage('Password must be at least 6 characters')
+];
+
+app.post("/register", userValidationRules, (req, res)=>{
+
+    const { email, password, confirm_password, chaining } = req.body;
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.render("register", { errors: errors.array(),  old: req.body });
+    }
+
+    res.redirect('/dashboard')
+});
 
 app.get("/dashboard", (req, res)=>{
     res.render("dashboard");
